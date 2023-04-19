74460e2455169d2dd05993f35f765bf7e000237c
niraj123744 <nirajnirmal661@gmail.com>
2023-04-19 23:57:54 +0530

    [Niraj] Add : This script is used for create a program prime number

---
+++ Prime_number.java
@@ -0,0 +1,28 @@
+package com.mypackage;
+
+import java.util.Scanner;
+
+public class Prime_number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num, i, count = 0;
+		Scanner s = new Scanner(System.in);
+
+		System.out.print("Enter a Number: ");
+		num = s.nextInt();
+
+		for (i = 2; i < num; i++) {
+			if (num % i == 0) {
+				count++;
+				break;
+			}
+		}
+
+		if (count == 0)
+			System.out.println("\nIt is a Prime Number.");
+		else
+			System.out.println("\nIt is not a Prime Number.");
+	}
+}
