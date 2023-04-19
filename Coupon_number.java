037a16db44d7f30ca4fde21543690714140a9b07
niraj123744 <nirajnirmal661@gmail.com>
2023-04-19 23:49:42 +0530

    [Niraj] Add : This script is used for create a program coupan number

---
+++ Coupon_number.java
@@ -0,0 +1,29 @@
+package com.mypackage;
+
+public class Coupon_number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// char array to get characters for coupen
+		char[] chars = "abcdefghijklmnop".toCharArray();
+
+		// length of coupen
+		int max = 100000000;
+
+		// random value
+		double d = Math.random();
+		int random = (int) (d * max);
+		String sb = "";
+		System.out.println("random double " + d + " random at start " + random);
+
+		while (random > 0) {
+			sb = sb + (chars[random % chars.length]);
+			random /= chars.length;
+			System.out.println(random + "  " + random % chars.length + " sb is " + sb);
+		}
+
+		String couponCode = sb.toString();
+		System.out.println("Coupon Code: " + couponCode);
+	}
+}
